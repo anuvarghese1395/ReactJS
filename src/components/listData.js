194407953f1c648492c3af3fae5494231fff4be9
anuvarghese1395 <anuvarghese1395@gmail.com>
2020-05-05 10:42:56 +0530

    hii

--- src/components/listData.js
+++ src/components/listData.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import PostData from '../data/weddingInvitation.json';
 
-class listData extends Component{
-    render(){
-        return(
+class listData extends Component {
+    render() {
+        return (
             <div>
                 <h1>Anu Varghese</h1>
-                {PostData.map((postDetail,index)=>{
-                    return <h1>{postDetail.name}</h1>
+                {PostData.map((postDetail,index) => {
+                   return <table>
+                        <tr>
+                            <th>Name</th>
+                            <th>RSVP</th>
+                            <th>plusone</th>
+                        </tr>
+                        <tr>
+                            <td>{postDetail.name}</td>
+                            <td>{postDetail.RSVP}</td>
+                            <td>{postDetail.pluseOne}</td>
+                        </tr>
+                    </table>
+                    // return <h1>{postDetail.name}</h1>
 
                 })}
             </div>
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import listData from '../components/listData';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Anu varghese project
-    </div>
-  );
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <listData />
+      </div>
+    )
+  }
 }
+
 
 export default App;
