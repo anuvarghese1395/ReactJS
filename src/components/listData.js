57d0cd501e578532cba5dffbb7b617d6fc53330a
anuvarghese1395 <anuvarghese1395@gmail.com>
2020-05-05 13:12:27 +0530

    change in listData

--- src/components/listData.js
+++ src/components/listData.js
@@ -1,27 +1,49 @@
 import React, { Component } from 'react';
 import PostData from '../data/weddingInvitation.json';
+import './listData.css';
 
 class listData extends Component {
+    //function to add the table header
+    renderTableHeader() {
+        let header = Object.keys(PostData[0])
+        return header.map((key, index) => {
+            return <th key={index}>{key.toUpperCase()}</th>
+        })
+    }
+    //function to add the table data
+    renderTableData() {
+        return (
+
+            PostData.map((postDetail, index) => {
+                const { id, name, RSVP, plusOne } = postDetail // destructuring instead of calling postDetail.name
+                return (
+                    <tr key={id}>
+                        <td>{id}</td>
+                        <td>{name}</td>
+                        <td>{RSVP}</td>
+                        <td>{plusOne}</td>
+
+                    </tr>
+                )
+
+
+
+
+            })
+
+        )
+    }
+    //main render function which calls the above data
     render() {
         return (
             <div>
-                <h1>Anu Varghese</h1>
-                {PostData.map((postDetail,index) => {
-                   return <table>
-                        <tr>
-                            <th>Name</th>
-                            <th>RSVP</th>
-                            <th>plusone</th>
-                        </tr>
-                        <tr>
-                            <td>{postDetail.name}</td>
-                            <td>{postDetail.RSVP}</td>
-                            <td>{postDetail.pluseOne}</td>
-                        </tr>
-                    </table>
-                    // return <h1>{postDetail.name}</h1>
-
-                })}
+                <h1 id='title'>React Dynamic Table</h1>
+                <table id='invitation'>
+                    <tbody>
+                        <tr>{this.renderTableHeader()}</tr>
+                        {this.renderTableData()}
+                    </tbody>
+                </table>
             </div>
         )
     }
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import listData from '../components/listData';
+import ListData from '../components/listData'
 import './App.css';
 
 class App extends Component {
   render(){
     return(
       <div>
-        <listData />
+        <ListData />
       </div>
     )
   }
