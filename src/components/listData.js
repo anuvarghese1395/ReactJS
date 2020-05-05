7bddf088f34b98de86a02b6282f47535e6cb70ff
anuvarghese1395 <anuvarghese1395@gmail.com>
2020-05-05 10:42:48 +0530

    hii

--- src/components/listData.js
+++ src/components/listData.js
@@ -1,1 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PostData from '../data/weddingInvitation.json';
+
+class listData extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Anu Varghese</h1>
+                {PostData.map((postDetail,index)=>{
+                    return <h1>{postDetail.name}</h1>
+
+                })}
+            </div>
+        )
+    }
+}
+
+export default listData;
