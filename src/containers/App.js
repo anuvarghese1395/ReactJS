53d8576f2dce1081b949691ac31c00a389e5423f
anuvarghese1395 <anuvarghese1395@gmail.com>
2020-05-05 18:01:36 +0530

    listData changed to invitationList

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ListData from '../components/listData'
+import ListData from '../components/invitationList'
 import './App.css';
 
 class App extends Component {
